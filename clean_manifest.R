66c0a2123b53d0c73a1922dbb9150437773db25f
kendomaniac <raffaele.calogero@gmail.com>
2020-09-02 21:15:15 +0200

    adding executing_docker_met_others.sh to be execute on the local folder that is scratch in docker

--- clean_manifest.R
+++ clean_manifest.R
@@ -1,8 +1,8 @@
-setwd("/Volumes/Elements/brain")
-tmp <- read.table("gdc_manifest_20200710_091301.txt", sep="\t", header=T, stringsAsFactors = F)
+setwd("/home/rcaloger/data/bronchus_and_lung_bam")
+tmp <- read.table("manifest.txt", sep="\t", header=T, stringsAsFactors = F)
 dim(tmp)
 downloaded <- dir(".")
 `%!in%` = Negate(`%in%`)
 tmp1 <- tmp[which(tmp$id %!in% downloaded),]
 dim(tmp1)
-write.table(tmp1, "gdc_manifest_20200710_091301bis.txt", sep="\t", row.names = F, quote=F)
+write.table(tmp1, "manifest_bis.txt", sep="\t", row.names = F, quote=F)
